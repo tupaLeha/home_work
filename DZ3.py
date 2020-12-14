8aa1ff6a745f7b191dfdbe33cc4f986eec76ab29
Paul Stetsenko <p.stetsenko@atomcream.com>
2020-12-14 20:19:20 +0200

    updated Fizz Buzz

--- DZ3.py
+++ DZ3.py
@@ -1,14 +1,34 @@
-fizz = int(input())
-buzz = int(input())
-fibu = int(input())
-i = 1
-while i <= fibu:
-    if((i % fizz == 0) and (i % buzz == 0)):
-        print('FB')
-    elif(i % buzz == 0):
-        print('B')
-    elif(i % fizz == 0):
-        print('F')
-    else:
-        print(i)
-    i = i + 1
+'''
+Третий уровень ("Мистер Буль. Джордж Буль!"):
+Решить задачу:
+
+Задача fizz-buzz:
+`
+У вас есть три числа, они вводятся из консоли. Первое число называется fizz, второе называется buzz.
+До третьего необходимо досчитать от единицы. Считая, надо выводить число.
+Если число кратно fizz - надо выводить F вместо числа.
+Если число кратно buzz - надо выводить B вместо числа.
+Если число кратно и fizz и buzz, надо выводить FB. И так - пока не будет достигнуто третье введенное число.
+
+Пример условия и результата:
+Введены числа 2, 5, 18
+Вывод должен быть таким:
+1 F 3 F B F 7 F 9 FB 11 F 13 F B F 17 F
+'''
+fizz = int(input('\nInput \'fizz\': '))
+buzz = int(input('Input \'buzz\': '))
+third = int(input('And now the third one: '))
+
+output = ''
+
+for i in range(1, third + 1):
+    result = ''
+    if not i % fizz:
+        result += 'F'
+    if not i % buzz:
+        result += 'B'
+    if not result:
+        result = i
+    output += f'{result} '
+
+print(f'\n The result is:\n {output}\n')
